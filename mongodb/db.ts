384f4f829973144457307b351354952f5266c56c
mohitjoer <[mohitjoe.r@gmail.com]>
2025-05-24 16:43:03 +0530

    commit	modified:   mongodb/db.ts

--- mongodb/db.ts
+++ mongodb/db.ts
@@ -1,24 +1,24 @@
 import mongoose from "mongoose"
 
-const connectionstring = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_USERNAME}@cluster-1.hp54uzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1`
 
-if (!connectionstring){
-    throw new Error ("please provide a valid connection string");
+const connectionString =`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster-1.hp54uzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1`
+
+if (!connectionString) {
+  throw new Error("Please provide a valid connection string");
 }
 
-const connectDB = async () =>{
-    if (mongoose.connection?.readyState >= 1){
-        
-        return;
-    }
+const connectDB = async () => {
+  if (mongoose.connection?.readyState >= 1) {
+    return;
+  }
 
-
-    try{
-        console.log("-----Connected To Database-----")
-    } catch (error) {
-        console.log("-----Error conneting to Database-----")
-        console.log(error);
-    }
+  try {
+    console.log("----Connecting to MongoDB----");
+    await mongoose.connect(connectionString);
+    console.log("----Connected to MongoDB----");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error);
+  }
 };
 
 export default connectDB;
